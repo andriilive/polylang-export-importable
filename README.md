2584d1de05fefe5d66e27e3380d621debc0461a0
Andy <43708848+andriilive@users.noreply.github.com>
2021-06-20 10:11:00 +0200

    Core Functionality: 1 language export, 2 langs import

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# polylang-export-importable
- Prepares CSV Files exported by PolyLang to import in WPML
+# polylang-export-importable-mo-po
+
+Makes importabe `.mo` and `.po` files form `polylang-translations.csv` exported with **PolyLang**
+
+Easy import this files with **WPML** or place them in your `/languages` folder
+
+Tested on `php@8.0`
+
+```shell
+# prepare: 
+# export each domain as csv file using polylang
+
+# install
+git clone https://github.com/andriilive/polylang-export-importable-mo-po.git && composer install
+
+# run
+php -c localhost:3000
+```
+
+## CSV import example
+```shell
+| String                   | "Source (plugin or theme)" | "Čeština (cs_CZ)"      | {...} |
+|--------------------------|----------------------------|------------------------|-------|
+| "Advanced Custom Fields" | acf                        | Pokročilá vlastní pole | ...   |
+| ...                      | ...                        | ...                    |       |
+```
+
+## Generated Locales example
+
+```shell
+├── acf_cs.mo
+├── acf_cs.po
+├── acf_sk.mo
+├── acf_sk.po
+├── all-in-one-seo-pack_cs.mo
+├── all-in-one-seo-pack_cs.po
+├── all-in-one-seo-pack_sk.mo
+└── all-in-one-seo-pack_sk.po
+```
+
+## Build on League\Csv && gettext/gettext
+
+https://packagist.org/packages/gettext/gettext
+https://csv.thephpleague.com/9.0/
+// 
+// 
--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
+// This script convers PolyLang CSV translations into mo and po files
+// Place separate csv files in /scr and run
+
 require __DIR__ . '/vendor/autoload.php';
 
 use League\Csv\Reader;
-use League\Csv\Statement;
-
-
+//use League\Csv\Statement;
+//use League\Csv\Writer;
 use Gettext\Translation;
+use Gettext\Translations;
 use Gettext\Generator\MoGenerator;
 use Gettext\Generator\PoGenerator;
+
+const LANGS = ['cs', 'sk'];
 
 $src = 'src/*.csv';
 $files = glob($src);
@@ -16,17 +21,26 @@ $files = glob($src);
 //var_dump($files);
 
 foreach ( $files as $file) {
-    $csv = Reader::createFromPath($file, 'r');
-    $csv->setDelimiter(';');
-    $csv->setHeaderOffset(1);
+    $domain = pathinfo($file, PATHINFO_FILENAME);
+    $translations = Translations::create($domain);
 
-    $stmt = Statement::create();
+    $reader = Reader::createFromPath($file, 'r');
+    $records = $reader->getRecords();
 
-
-    $records = $stmt->process($csv);
-
-    foreach ($records as $record) {
-        //do something here
+    foreach ($records as $offset => $record) {
+        $translation = Translation::create(null, $record[0]);
+        $translation->translate($record[0]);
+        $translations->add($translation);
     }
 
+    //Load a PO file
+    $poGenerator = new PoGenerator();
+    $moGenerator = new MoGenerator();
+
+    foreach ( LANGS as $LANG ) {
+        $pofile = 'locales/'.$domain.'_'.$LANG.'.po';
+        $mofile = 'locales/'.$domain.'_'.$LANG.'.mo';
+        $poGenerator->generateFile($translations, $pofile);
+        $moGenerator->generateFile($translations, $mofile);
+    }
 }
